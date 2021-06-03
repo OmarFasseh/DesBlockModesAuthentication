0df6ca8685977b7a5fc4d3ed05fac352eaf60094
Gaber <ahmed.gaber428@yahoo.com>
2021-06-03 18:49:05 +0200

    Hmac initial

--- src/md5main.cc
+++ src/md5main.cc
@@ -1,13 +1,26 @@
 #include <cstdio>
 #include <cstring>
 #include "md5.hh"
-
-int main(int argc, char*argv[]) {
-    if(argc != 2) {
-        puts("Usage: md5 <string>");
-        return 1;
-    }
-    printf("md5sum : %s\n", md5sum( argv[1], strlen(argv[1])).c_str());
-    printf("md5sum6: %s\n", md5sum6(argv[1], strlen(argv[1])).c_str());
-    return 0;
+#include<string>
+#include"desBlockModes.h"
+#include"des.h"
+using namespace std;
+int main() {
+   	string k = "0000000000000000";
+	string message = "hellowor";
+	string ipad="3636363636363636";
+	string opad = "5c5c5c5c5c5c5c5c";
+	string si = bin2Hex(xorS(hex2Bin(k), hex2Bin(ipad)));
+	
+	
+	message = hex2String(si) + message;
+	message = string2Hex(message);
+	string hash = md5sum(message.c_str(), strlen(message.c_str()));
+	
+	
+	string so = bin2Hex(xorS(hex2Bin(k), hex2Bin(opad)));
+	hash = hex2String(so) + hex2String(hash);
+	string HMAC = md5sum(hash.c_str(), strlen(hash.c_str()));
+	//cout << HMAC;
+	return 0 ;
 }
