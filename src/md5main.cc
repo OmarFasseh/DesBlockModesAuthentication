a6a83975edfcf55dda58823d0b1ec89258737c38
Omar Fasseh <okomarkhaled@gmail.com>
2021-06-04 03:03:19 +0200

    sha1 added

--- src/md5main.cc
+++ src/md5main.cc
@@ -1,26 +1,53 @@
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 #include "md5.hh"
-#include<string>
-#include"desBlockModes.h"
-#include"des.h"
+#include <string>
+#include "sha1.hpp"
+#include "desBlockModes.h"
+#include "des.h"
 using namespace std;
-int main() {
-   	string k = "0000000000000000";
-	string message = "hellowor";
-	string ipad="3636363636363636";
-	string opad = "5c5c5c5c5c5c5c5c";
+
+#define B 512
+#define BHex 128
+
+int main()
+{
+
+	SHA1 checksum;
+	// checksum.update("tmp");
+	// string hash = checksum.final();
+	// checksum.update("tmp2");
+	// cout <<" t1 " <<hash <<endl;
+	// hash = checksum.final();
+	// cout <<" t1 " <<hash <<endl;
+
+
+
+	string k = "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001";
+	string message = "helloworldhelloworldhelloworldhelloworldhelloworldhelloworld1234";
+	string ipad = "36363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636363636";
+	string opad = "5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c";
 	string si = bin2Hex(xorS(hex2Bin(k), hex2Bin(ipad)));
-	
-	
+
+	string tmp;
 	message = hex2String(si) + message;
-	message = string2Hex(message);
-	string hash = md5sum(message.c_str(), strlen(message.c_str()));
-	
-	
+	//message = string2Hex(message);
+	cout << si << "   " << message << endl;
+
+	checksum.update(message);
+	string hash = checksum.final();
+	// hash = md5sum(message.c_str(), message.length());
+	cout << "hash: " << hash << endl;
+	hash = string(BHex-hash.size(), '0') + hash;
+	cout << "after padding: " << hash << endl;
+
 	string so = bin2Hex(xorS(hex2Bin(k), hex2Bin(opad)));
-	hash = hex2String(so) + hex2String(hash);
-	string HMAC = md5sum(hash.c_str(), strlen(hash.c_str()));
-	//cout << HMAC;
-	return 0 ;
+	tmp = hex2String(so+hash);
+	checksum.update(tmp);
+	//const string hash = checksum.final();
+	//string HMAC = md5sum(hash.c_str(), strlen(hash.c_str()));
+	cout << checksum.final() << endl;
+	//cout << HMAC << endl;
+	return 0;
 }
